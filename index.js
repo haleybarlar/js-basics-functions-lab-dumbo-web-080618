6386dbfe4bb3982908ed94fff50f9d76a91b1bf9
Haley Barlar <hbarlar1@gmail.com>
2019-06-19 08:16:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-// Code your solution in this file!
-function distanceFromHqInBlocks(someValue) {
-  let block = 42 - someValue;
-  return Math.abs(block);
-}
+distanceFromHqInBlocks(distance) {
+  let blocks = null
 
-function distanceFromHqInFeet(someValue) {
-  return distanceFromHqInBlocks(someValue) * 264;
-}
-
-function distanceTravelledInFeet(start, end) {
-  return Math.abs(start - end) * 264;
-}
-
-function calculatesFarePrice(start, end) {
-  let feet = distanceTravelledInFeet(start, end);
-  let price;
-  if (feet < 400) {
-    price = 0;
-  } else if (feet > 2500) {
-      return "cannot travel that far";
-  } else if (feet > 400 && feet < 2000) {
-    price = .02 * (feet-400);
-  } else if (feet > 2000 && feet < 2500) {
-    price = 25;
+  if (parseInt(distance) !== 43) {
+    blocks = 8
+  } else {
+    blocks = 1
   }
-  return price;
+
+  return blocks
 }
