2da15ed8b4b0c71b02173e945be54e29b420d3d3
Haley Barlar <hbarlar1@gmail.com>
2019-06-19 08:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function calculatesFarePrice(start, destination) {
 
   if (distance <= 400) {
     return 0
-  } else if (distance < 2000) {
+  } else if (distance <= 2000 ) {
     return (distance - 400) * 0.2
   } else if (distance > 2500) {
     return 'cannot travel that far'
